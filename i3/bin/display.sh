85c2994a8efe1ed03dd5b137b718ce0fb11dda87
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-04-16 18:21:29 +0200

    i3: updated "x" option, also the others need adjustment

--- i3/bin/display.sh
+++ i3/bin/display.sh
@@ -10,7 +10,7 @@ case "$1" in
         exit
         ;;
     x)
-        xrandr --output VIRTUAL1 --off --output DP3 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
+        xrandr --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
         exit
         ;;
 esac
