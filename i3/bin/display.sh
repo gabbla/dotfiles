36aa37a1c4d6a2c86bc7c33573fb62e377c6344e
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-04-17 01:46:33 +0200

    i3: screen menu updated

--- i3/bin/display.sh
+++ i3/bin/display.sh
@@ -2,11 +2,11 @@
 
 case "$1" in
     i)
-        xrandr --output VIRTUAL1 --off --output DP3 --off --output DP1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
+        xrandr --output DP-3 --off --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
         exit
         ;;
     e)
-        xrandr --output VIRTUAL1 --off --output DP3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --off --output DP2 --off
+        xrandr --output DP-3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --off --output DP-2 --off
         exit
         ;;
     x)
