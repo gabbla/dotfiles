cc09417d6fbfd58675b9f491cb36549bb8b5ef5f
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-08-05 22:15:19 +0200

    i3: display updated

--- i3/bin/display.sh
+++ i3/bin/display.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+INTERNAL="eDP1"
+EXTERNAL="DP3"
+
 case "$1" in
-    i)
-        xrandr --output DP-3 --off --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
+    i) # internal
+        xrandr --output $EXTERNAL --off --output DP1 --off --output $INTERNAL --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
         exit
         ;;
-    e)
-        xrandr --output DP-3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --off --output DP-2 --off
+    e) # external
+        xrandr --output $EXTERNAL --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output $INTERNAL --off --output DP2 --off
         exit
         ;;
-    x)
-        xrandr --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
+    x) #extend
+        xrandr --output $EXTERNAL --mode auto
+        xrandr --output $EXTERNAL --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output $INTERNAL --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
         exit
         ;;
 esac
+
+# TODO restart polybar
