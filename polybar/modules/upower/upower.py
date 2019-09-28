b40f4ec18a1045bc36dc3c8c75475485b2d03360
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-09-29 01:35:53 +0200

    polybar: module: upower: added doc

--- polybar/modules/upower/upower.py
+++ polybar/modules/upower/upower.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 
 # Usage: upower.py <device> <icon>
+# This script is meant to be used as polybar module. It prints the 'percentage'
+# property of a device to stdout. Nothing elaborated, just read the property
+# through dbus on upowerd.
+# The 'device' arg is taken from `upower --enumerate` last field
+# Ex:
+# upower --eumerate
+# /org/freedesktop/UPower/devices/line_power_AC
+#                                 ^^^^^^^^^^^^^ Use this as device
+# The 'icon' is printed before the percentage. Can be any string
+# Example in polybar/config:
+# [module/mx_battery]
+# type = custom/script
+# exec = ~/.dotfiles/polybar/modules/upower/upower.py mouse_dev_F2_B2_22_11_37_09 
 
 import dbus
 import sys
