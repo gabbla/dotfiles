adf278ac2cbecd3c59adb37ab7121c87f233017d
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-09-29 01:27:11 +0200

    polybar: added upower module

--- polybar/modules/upower/upower.py
+++ polybar/modules/upower/upower.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 
-# Usage: upower.py <device> <label>
+# Usage: upower.py <device> <icon>
 
 import dbus
+import sys
+
 try:
+    device_name = "/org/freedesktop/UPower/devices/{}".format(sys.argv[1])
     bus = dbus.SystemBus()
-    device = bus.get_object("org.freedesktop.UPower", "/org/freedesktop/UPower/devices/battery_BAT0")
+    device = bus.get_object("org.freedesktop.UPower", device_name)
     device_ifc = dbus.Interface(device, dbus_interface='org.freedesktop.DBus.Properties')
 
     bat_level = device_ifc.Get('org.freedesktop.UPower.Device', 'Percentage')
+    print("{} {}%".format(sys.argv[2], int(bat_level)))
+
 except Exception:
     exit
